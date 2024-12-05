bc3a0542e1db40978f02c7d916a8a141895e0381
Joseph <threlkeld@mines.edu>
2024-12-04 18:23:25 -0700

    day 2 complete

--- Days/2/solution2.hs
+++ Days/2/solution2.hs
@@ -1,5 +1,6 @@
 import GHC.Base (TrName(TrNameD))
 import Language.Haskell.TH (safe)
+import Data.List (subsequences)
 
 diffs [] = []
 diffs xs = zipWith (-) (tail xs) xs
@@ -12,11 +13,15 @@ safeReport [x]  = True
 safeReport xs = all (\a -> signsMatch d a && abs a < 4 && a /= 0) (d:ds)
     where (d:ds) = diffs xs
 
+tryAllSubs xs =
+    any safeReport . filter (\l -> length l == length xs - 1) $ subsequences xs
 
 -- Function to try safeReport on all sub lists missing one element
 
+
+
 main = 
-    print . length . filter safeReport . map (map readInt . words) . lines =<< readFile "input.txt" 
+    print . length . filter (\l -> safeReport l || tryAllSubs l) . map (map readInt . words) . lines =<< readFile "input.txt" 
 
 readInt :: String -> Int
 readInt = read
